8e636dae50f7d5db4bd4669df47a5fa21687293a
Leo <conceicao.leonardo9@gmail.com>
2023-03-17 19:24:02 -0300

    relatorio em Html

--- features/support/env.rb
+++ features/support/env.rb
@@ -3,12 +3,14 @@ require 'selenium-webdriver'
 require 'site_prism'
 require 'rspec'
 require_relative 'helper.rb'
+require_relative 'screenshot.rb'
 
 World(PageObjects)
+World(Screenshot)
 
 Capybara.configure do |config|
   #selenium   selenium_chrome  selenium_chrome_headless
   config.default_driver = :selenium_chrome
+  Capybara.page.driver.browser.manage.window.maximize
   config.default_max_wait_time = 5
-
 end
